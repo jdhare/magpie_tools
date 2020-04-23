TS updates: fixed definitoion of scattering vector, fixed calculation of alpha

code/magpie_data.py
@@ -14,7 +14,7 @@ from copy import copy
 from ipywidgets import interact, interactive, fixed, FloatProgress
 import ipywidgets as widgets
 from IPython.display import display
-import cv2
+#import cv2
 from lmfit.models import GaussianModel, ConstantModel
 from mpl_toolkits.axes_grid1 import ImageGrid
 
@@ -62,7 +62,7 @@ class DataMap:
         self.lo=profile_line(self.data_c, start_px,end_px,linewidth=lineout_width_mm*self.scale)
         #set up a mm scale centred on 0
         px_range=self.lo.size/2
-        self.mm=np.linspace(-px_range, px_range, 2*px_range)/self.scale #flip range to match images
+        self.mm=np.linspace(-px_range, px_range, self.lo.size)/self.scale #flip range to match images
     def plot_lineout(self, ax=None, label='', multiply_by=1):
         if ax is None:
             fig, ax=plt.subplots(figsize=(12,8))

code/plasma_parameter_calculator.py
@@ -245,8 +245,8 @@ def col_log(a,b, T_e=None):
         elif Ti<TZ and TZ<Te:
             print('Ion-Electron, T_i*m_e/m_i<10 Z^2<T_e')
             return 24-np.log(el.n**0.5*el.T**-1.0)
-        elif Tie<io.Z*Ti:
-            print('Ion-Electron, T_i*m_e/m_i<10 Z^2<T_e')
+        elif Te<io.Z*Ti:
+            print('SUSPECT! Ion-Electron, T_i*m_e/m_i<10 Z^2<T_e')
             return 30-np.log(io.n**0.5*io.T**-1.5*io.Z**2/io.m)
         else:
             print('Ion-Electron: Whoops! You broke Physics!!!')

code/spectral_density_functions.py
@@ -7,7 +7,7 @@ m_e=scipy.constants.m_e
 m_p=scipy.constants.m_p
 e=scipy.constants.e
 c=scipy.constants.c
-epsilon_0=scipy.constants.ep
+epsilon_0=scipy.constants.epsilon_0
 
 exp=np.exp
 Il=scipy.special.iv #modified bessel function of first kind
@@ -495,13 +495,15 @@ def S_k_omega_nLTE(lambda_range, lambda_in, theta,  Z_Te_table, T_e, T_i, n_e, A
     m_i=m_p*A
     om_pe=5.64e4*n_e**0.5
     #define omega and k as in Sheffield 113
-    ki=2*pi/lambda_in
-    omega_i=((c*ki)**2+om_pe**2)**0.5
-    ks=2*pi/lambda_range
-    omega_s=((c*ks)**2+om_pe**2)**0.5
+    omega_i = 2*pi/lambda_in * c #input free space frequency
+    ki = ((omega_i**2 - om_pe**2)/c**2)**0.5 #input wave-vector in plasma
+
+    omega_s = 2*pi/lambda_range * c #scattering free space frequency
+    ks = ((omega_s**2 - om_pe**2)/c**2)**0.5 #scattering wave-vector in plasma
+
     th=theta/180.0*np.pi
     k=(ks**2+ki**2-2*ks*ki*np.cos(th))**0.5
-    omega=omega_s-omega_i
+    omega=omega_s-omega_i #frequency shift
     #define dimensionless parameters ala Sheffield
     a=sqrt(2*e*T_e/m_e)
     b=sqrt(2*e*T_i/m_i)

code/ts_analysis.py
@@ -138,7 +138,8 @@ class Fibre:
 
     def calculate_alpha(self):
         lambda_De=7.43*(self.params['T_e']/self.params['n_e'])**0.5 #in m
-        k=4*np.pi*np.sin(self.theta/2.0)/self.l0
+        th=self.theta*np.pi/180
+        k=4*np.pi*np.sin(th/2.0)/self.l0
         self.params['alpha']=np.abs(1/(k*lambda_De))
     def calculate_predicted_intensity(self):
         #calculate the expected TS intensity